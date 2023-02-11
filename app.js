201efe510bc716c108772173682440bb3db3d8a8
Maxim <shidloba@gmail.com>
2023-02-12 01:10:22 +0300

    update_css

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ setInterval(() => {
     let hh = day.getHours() * 30;
     let mm = day.getMinutes() * deg;
     let ss = day.getSeconds() * deg;
-    
+
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;

ef0f7b4e8978bee7d8a14f618a8a427636694577
Maxim <shidloba@gmail.com>
2023-03-04 22:20:33 +0300

    update watch to class

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const hr = document.querySelector('#hr');
-const mn = document.querySelector('#mn');
-const sc = document.querySelector('#sc');
-setInterval(() => {
+class Whatch {
 
-    let day = new Date();
-    let hh = day.getHours() * 30;
-    let mm = day.getMinutes() * 6;
-    let ss = day.getSeconds() * 6;
-    
+  constructor() {
+    setInterval(this.doTime, 1000)
+  }
+
+  doTime() {
+    let time = new Date();
+    let hh = time.getHours() * 30;
+    let mm = time.getMinutes() * 6;
+    let ss = time.getSeconds() * 6;
+
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
-})
+  }
+}
+
+const test = new Whatch()
